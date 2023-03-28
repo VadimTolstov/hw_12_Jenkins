af2bf24f419066d9de2eeb40631de399b300314d
VadimTolstov <tolstov50@mail.ru>
2023-03-28 20:10:33 +0300

    v 0.5

--- src/test/java/hw_12_Jenkins/TestBaseFaker.java
+++ src/test/java/hw_12_Jenkins/TestBaseFaker.java
@@ -2,13 +2,26 @@ package hw_12_Jenkins;
 
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Map;
 
 public class TestBaseFaker {
 
     @BeforeAll
     static void beforeAll() {
-        Configuration.browserSize = "1920x1080";
-//        Configuration.holdBrowserOpen = true;
         Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browser = "chrome";
+        Configuration.browserVersion = "100.0";
+        Configuration.browserSize = "1920x1080";
+        //подлючаем
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        //Включаем VNC окошка в окошке и  включаем запись видео Video
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+        Configuration.browserCapabilities = capabilities;
     }
 }
